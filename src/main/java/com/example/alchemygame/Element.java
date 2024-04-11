13ff607e19f9fb546ab41d22c820e91b4bbcc047
ecdeans <evadeansnc@gmail.com>
2024-04-11 16:24:09 -0400

    add more to element class skeleton

--- src/main/java/com/example/alchemygame/Element.java
+++ src/main/java/com/example/alchemygame/Element.java
@@ -2,10 +2,18 @@ package com.example.alchemygame;
 
 public class Element {
 
-    private String elementName;
+    private final String elementName;
+
+    public Element(String elementName) {
+        this.elementName = elementName;
+    }
 
     public void reactOnCollision(Element otherElement) {
         // add reaction here, this called when elements collide
     }
-    
+
+    public String getElementName() {
+        return this.elementName;
+    }
+
 }
