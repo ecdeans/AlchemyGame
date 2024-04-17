84801fc2a2f50ebdf1e3341a59d55c5cac5f2359
ecdeans <evadeansnc@gmail.com>
2024-04-17 11:07:29 -0400

    Added FileReader class that puts .csv into an Array and .csv file for testing

---
+++ src/main/java/com/example/alchemygame/FileReader.java
@@ -0,0 +1,30 @@
+package com.example.alchemygame;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+    public static String[][] getArray() {
+        String filePath = "src/main/resources/ElementCombinations.csv";
+        return readCSV(filePath);
+    }
+
+   public static String[][] readCSV(String filePath) {
+        List<String[]> rows = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                rows.add(values);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rows.toArray(new String[0][]);
+    }
+}
+
